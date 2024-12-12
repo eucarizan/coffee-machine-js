43c317b56592cd626e9023c9f86c68f40834cbd4
eucarizan <eucarizan@gmail.com>
2024-12-12 21:44:43 +0800

    stage 3 done

--- copy-03.js
+++ copy-03.js
@@ -1,15 +1,22 @@
-const input = require('sync-input');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output:process.stdout
+});
 
 let water = 0;
 let milk = 0;
 let beans = 0;
 let cups = 0;
 
-function getSupply() {
-  water = input("Write how many ml of water the coffee machine has:");
-  milk = input("Write how many ml of milk the coffee machine has:");
-  beans = input("Write how many grams of coffee beans the coffee machine has:");
-  cups = input("Write how many cups of coffee you will need:");
+async function getSupply() {
+  const question = (query) => new Promise((resolve) => rl.question(query, resolve));
+  
+  water = await question("Write how many ml of water the coffee machine has:");
+  milk = await question("Write how many ml of milk the coffee machine has:");
+  beans = await question("Write how many grams of coffee beans the coffee machine has:");
+  cups = await question("Write how many cups of coffee you will need:");
+  rl.close();
 }
 
 function calculate(waterAvailable, milkAvailable, beansAvailable) {
@@ -28,7 +35,7 @@ function makeCoffee(cups, maxCups) {
   let output = "";
   if (maxCups >= cups) {
     output += "Yes, I can make that amount of coffee";
-    if (maxCups - 1 > 1) {
+    if (maxCups > 1) {
       output += ` (and even ${maxCups - 1} more than that)`;
     }
   } else {
@@ -37,6 +44,8 @@ function makeCoffee(cups, maxCups) {
   console.log(output);
 }
 
-getSupply();
-const maxCups = calculate(water, milk, beans);
-makeCoffee(cups, maxCups);
+(async function main() {
+  await getSupply();
+  const maxCups = calculate(water, milk, beans);
+  makeCoffee(cups, maxCups);
+})();
